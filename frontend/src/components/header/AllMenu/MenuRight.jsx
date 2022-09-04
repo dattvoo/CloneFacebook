461815113c115d37f5a8c82e9e445a194d888d8e
Dat Vo <datvodat288@gmail.com>
2022-09-04 15:08:48 +0700

    feat all menu

---
+++ frontend/src/components/header/AllMenu/MenuRight.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { create } from "../../../constant/Menu";
+export const MenuRight = () => {
+  return (
+    <div className="all_right">
+      <div className="all_right_header">Create</div>
+      {create.map((item, index) => (
+        <div className="all_right_item hover1">
+          <div className="all_right_circle">
+            <i className={item.icon}></i>
+          </div>
+          {item.name}
+        </div>
+      ))}
+    </div>
+  );
+};
