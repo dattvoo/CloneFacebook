dd64d16d95a7593d153e6c20a346ce1139b93747
Dat Vo <65235281+sunkidd06@users.noreply.github.com>
2022-09-17 10:25:54 +0700

    feat/work on story (#16)

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -4,12 +4,17 @@ import "./responsive.css";
 import { LeftHome } from "../../components/home/Left";
 import { useSelector } from "react-redux";
 import { RightHome } from "../../components/home/Right";
+import { Stories } from "../../components/home/stories";
+import "./style.css"; 
 export default function Home() {
   const user = useSelector(state => state.user);
   return (
-    <div>
+    <div className="home">
       <Header />
       <LeftHome user={user} />
+      <div className="home_middle">
+        <Stories/>
+      </div>
       <RightHome user={user}/>
     </div>
   );
