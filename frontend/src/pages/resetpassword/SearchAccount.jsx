1ee262f342e04559d74f576fef99c94e20a78338
Dat Vo <65235281+dattvoo@users.noreply.github.com>
2022-10-18 23:59:59 +0700

    Responsive for SearchAccount (#26)

--- frontend/src/pages/resetpassword/SearchAccount.jsx
+++ frontend/src/pages/resetpassword/SearchAccount.jsx
@@ -1,9 +1,16 @@
 import { Form, Formik } from "formik";
 import React, { useState } from "react";
 import LoginInput from "../../components/inputs/loginInput";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import * as yup from "yup";
 export const SearchAccount = (props) => {
-    const {email, error, setEmail} = props;
+  const { email, error, setEmail } = props;
+  const validateEmail = yup.object({
+    email: yup.string()
+    .required("Email address is required")
+    .email("Must be a valid email address")
+    .max("50", "Email address can't be more than 50 characters")
+  })
   return (
     <div className="reset_form">
       <div className="reset_form_header">Find Your Account</div>
@@ -11,7 +18,10 @@ export const SearchAccount = (props) => {
         Please enter your mail address or mobile number to search for your
         account.
       </div>
-      <Formik enableReinitialize initialValues={{ email }}>
+      <Formik enableReinitialize 
+      initialValues={{ email }}
+      validationSchema={validateEmail}
+      >
         {(formik) => (
           <Form>
             <LoginInput
