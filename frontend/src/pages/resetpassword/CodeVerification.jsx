eebeb7e48421d7fb6791a4cb11b87e98e21839e5
Dat Vo <65235281+dattvoo@users.noreply.github.com>
2022-10-21 23:04:50 +0700

    fix validate reset password (#27)

--- frontend/src/pages/resetpassword/CodeVerification.jsx
+++ frontend/src/pages/resetpassword/CodeVerification.jsx
@@ -2,15 +2,27 @@ import { Form, Formik } from "formik";
 import React, { useState } from "react";
 import LoginInput from "../../components/inputs/loginInput";
 import { Link } from "react-router-dom";
+import * as yup from "yup";
 export const CodeVerification = (props) => {
   const { code, error, setCode } = props;
+  const validateCode = yup.object({
+    code: yup
+      .string()
+      .required("Code is required")
+      .min("5", "Code must be 5 character")
+      .max("5", "Code must be 5 character")
+  });
   return (
     <div className="reset_form">
       <div className="reset_form_header">Code Verification</div>
       <div className="reset_form_text">
         Please enter code that been sent to your email.
       </div>
-      <Formik enableReinitialize initialValues={{ code }}>
+      <Formik
+        enableReinitialize
+        initialValues={{ code }}
+        validationSchema={validateCode}
+      >
         {(formik) => (
           <Form>
             <LoginInput
