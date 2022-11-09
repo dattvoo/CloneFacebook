59eb5b676af5a5e827a80ef013322f2f4fa249bf
Dat Vo <datvodat288@gmail.com>
2022-11-09 14:16:21 +0700

    feat/Upload_Image_To_Cloudinary, add Cloudinary Libary

--- backend/controllers/upLoadController.js
+++ backend/controllers/upLoadController.js
@@ -1,8 +1,55 @@
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
-exports.uploadImages = async (req, res, next) => {
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+
+})
+
+exports.uploadImages = async (req, res) => {
     try {
-        return res.json("Welcom to my chanel")
+        const { path } = req.body;
+        let files = Object.values(req.files).flat();
+        let images = [];
+        for (const file of files) {
+            const url = await uploadToCloudinary(file, path);
+            images.push(url);
+            removetmp(file.tempFilePath)
+        }
+        res.json(images);
     } catch (error) {
         return res.status(400).json({ message: error.message })
     }
 }
+
+const uploadToCloudinary = async (file, path) => {
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload(
+            file.tempFilePath,
+            {
+                folder: path,
+            },
+            (err, res) => {
+                if (err) {
+                    removetmp(file.tempFilePath);
+                    return res.status(400).json({ message: "Upload image failed" });
+                }
+                resolve({
+                    url: res.secure_url
+                })
+            }
+        )
+    })
+}
+
+
+const removetmp = (path) => {
+    fs.unlink(path, (err) => {
+        if (err)
+            throw err;
+    })
+}
+
+
