c7f1f238ba16bcbfc9c87f4cf32ee88aa9c02f98
Dat Vo <65235281+dattvoo@users.noreply.github.com>
2022-11-16 14:22:28 +0700

    feat/Submit_Post_With_Background (#42)

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -3,6 +3,7 @@ const PostModel = require("../models/PostModel");
 exports.createPost = async (req, res) => {
     try {
         const newPost = await new PostModel(req.body).save();
+        return res.send(newPost);
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
